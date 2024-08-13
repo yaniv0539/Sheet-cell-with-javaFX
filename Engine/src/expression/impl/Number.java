ea4298a5658e2292d15279b17fb11c84d5c54001
raviv5 <itayraviv5@gmail.com>
2024-08-13 13:40:19 +0300

    added a subs interfaces

--- Engine/src/expression/impl/Number.java
+++ Engine/src/expression/impl/Number.java
@@ -22,4 +22,8 @@ public class Number implements Expression<Double>,NumericExpression {
         return "";
     }
 
+    @Override
+    public String toString() {
+        return value >= 0 ? Double.toString(value): "(" + Double.toString(value) + ")";
+    }
 }
