1149a078928b959b525bb6da18d09379316b82a0
raviv5 <itayraviv5@gmail.com>
2024-09-06 17:12:34 +0300

    header and sheet fxml

---
+++ UI/src/header/HeaderController.java
@@ -0,0 +1,66 @@
+package header;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HeaderController {
+
+        @FXML
+        private Button buttonUpdateCell;
+
+        @FXML
+        private Button buttonUploadXmlFile;
+
+        @FXML
+        private Label labelCellId;
+
+        @FXML
+        private Label labelOriginalValue;
+
+        @FXML
+        private Label labelVersionSelector;
+
+        @FXML
+        private Label lableFileName;
+
+        @FXML
+        private TextField textFieldCellId;
+
+        @FXML
+        private TextField textFieldFileName;
+
+        @FXML
+        private TextField textFieldOrignalValue;
+
+        @FXML
+        private TextField textFieldVersionSelector;
+
+        @FXML
+        void buttonUpdateCellAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void buttonUploadXmlFileAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void textFieldFileNameAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void textFieldOrignalValueAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void textFieldVersionSelectorAction(ActionEvent event) {
+
+        }
+
+    }
